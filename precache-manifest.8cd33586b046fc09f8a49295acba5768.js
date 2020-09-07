7f56404e8fdde2d4eba42e3a0a99af0f45343a38
RonaldH-7 <ronald.hua7@gmail.com>
2020-09-07 13:23:46 -0400

    Updates

---
+++ precache-manifest.8cd33586b046fc09f8a49295acba5768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ba3872ad1be813767af74a4e8d7c7a",
+    "url": "/Shoppies/index.html"
+  },
+  {
+    "revision": "0d9c202df772a567a25a",
+    "url": "/Shoppies/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a8ac11bdd6b459169486",
+    "url": "/Shoppies/static/css/main.6e6a9a35.chunk.css"
+  },
+  {
+    "revision": "0d9c202df772a567a25a",
+    "url": "/Shoppies/static/js/2.9af097bb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Shoppies/static/js/2.9af097bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ac11bdd6b459169486",
+    "url": "/Shoppies/static/js/main.1e47f3b8.chunk.js"
+  },
+  {
+    "revision": "2c0e58da846e62c6c06a",
+    "url": "/Shoppies/static/js/runtime-main.b439450a.js"
+  }
+]);
