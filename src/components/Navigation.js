570d412dc59e0f23c66ad6cbd0c3c61f1148a811
RonaldH-7 <ronald.hua7@gmail.com>
2020-09-10 16:41:12 -0400

    Removed unnecessary comments

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,33 +1,3 @@
-// import React from 'react';
-// import Nav from 'react-bootstrap/Nav';
-// import { Link } from 'react-router-dom';
-
-// class Navigation extends React.Component {
-//     render() {
-//         return (
-//             <Nav>
-//                 <Nav.Item>
-//                     <Link className="nav-link" to="/">
-//                         Home
-//                     </Link>
-//                 </Nav.Item>
-//                 <Nav.Item>
-//                     <Link className="nav-link" to="/about">
-//                         About
-//                     </Link>
-//                 </Nav.Item>
-//                 <Nav.Item>
-//                     <Link className="nav-link" to="/contact">
-//                         Contact
-//                     </Link>
-//                 </Nav.Item>
-//             </Nav>
-//         );
-//     }
-// }
-
-// export default Navigation;
-
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -37,7 +7,9 @@ class Navigation extends React.Component {
     render() {
         return (
             <Navbar className="nav-bar" expand="lg">
-                <Navbar.Brand href="#home" className="logo"><img src={require('../images/shoppies_logo.png')} alt="Shoppies logo" style={{width: "40px", height: "auto"}} /></Navbar.Brand>
+                <Navbar.Brand href="#home" className="logo">
+                    <img src={require('../images/shoppies_logo.png')} alt="Shoppies logo" style={{width: "40px", height: "auto"}} />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav>
